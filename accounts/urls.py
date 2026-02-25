59ad2e63099da40ae9dbc239107649a3e4deaa6b
capsunlock <capsonluck@gmail.com>
2026-02-25 22:45:14 +0100

    Wrapped up the Authentication System

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path ('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
+    # Login endpoint
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # For when the login expires
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
