75c005f6d694819bc8311ede8414c9df17f2d23e
capsunlock <capsonluck@gmail.com>
2026-02-25 22:37:37 +0100

    fixxed what was wrong with S.V.U - accounts push

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.response import AllowAny
+from rest_framework.permissions import AllowAny 
 from .serializers import RegisterSerializer
 
 
